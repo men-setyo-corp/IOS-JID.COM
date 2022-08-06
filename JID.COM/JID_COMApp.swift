9965854d2ae89a249f0815af097ba4536048ff8d
Fifit setyo <35519224+setyofft@users.noreply.github.com>
2022-08-06 22:29:12 +0700

    Initial Commit

---
+++ JID.COM/JID_COMApp.swift
@@ -0,0 +1,17 @@
+//
+//  JID_COMApp.swift
+//  JID.COM
+//
+//  Created by Macbook on 06/08/22.
+//
+
+import SwiftUI
+
+@main
+struct JID_COMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
