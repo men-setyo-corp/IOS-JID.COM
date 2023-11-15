1e968c866d0fbbb8ccdc12c596844da84d5c5d50
Fifit setyo <35519224+setyofft@users.noreply.github.com>
2023-11-16 06:31:44 +0700

    .

---
+++ JID.COM/Models/DashboardLalinModel.swift
@@ -0,0 +1,37 @@
+//
+//  DashboardLalinModel.swift
+//  JID.COM
+//
+//  Created by Panda on 15/11/23.
+//
+
+import UIKit
+import MapboxMaps
+import SwiftyJSON
+import SwiftUI
+
+class DashboardLalinModel: ObservableObject{
+    // start class
+    func getDataDashRekayasa(completion: @escaping (JSON) -> ())  {
+        DispatchQueue.global().async {
+            RestApiController().resAPIDevGet(endPoint: "dashboard_lalin/v1/countRekayasa", method: .get) { (results) in
+                let getJSON = JSON(results ?? "Null data from API")
+                DispatchQueue.main.async {
+                    if getJSON["status"].boolValue {
+                        do {
+                            let jsonDataTh = getJSON["data"]["data"]
+                            let jsonDataHari = getJSON["data"]["dataDaily"]
+                            let decodedSentences = JSON(
+                                ["tahun": jsonDataTh, "hari": jsonDataHari]
+                            )
+                            completion(decodedSentences)
+                        }
+                    }else{
+                        print(getJSON["msg"].stringValue)
+                    }
+                }
+            }
+        }
+    }
+    // end class
+}
--- JID.COM/Views/Lalu Lintas/DashLaluLintas.swift
+++ JID.COM/Views/Lalu Lintas/DashLaluLintas.swift
@@ -6,10 +6,154 @@
 //
 
 import SwiftUI
+import SwiftyJSON
 
 struct DashLaluLintas: View {
+    private let data: [Int] = Array(1...3)
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    @State var dataRekayasa : JSON = []
+    @State var oneway: String = "0"
+    @State var contraflow: String = "0"
+    @State var pengalihan: String = "0"
+    
+    @State var selectHari: Bool = true
+    @State var selectTahun: Bool = false
+    @State var selectDefault: String = "hari"
+    
+    @State var dateNow: String = ""
+    
     var body: some View {
-        Text("Dashboard Lalu Lintas")
+        VStack(alignment: .leading){
+            HStack{
+                Text("Filter")
+                    .font(.system(size: 13))
+                Spacer()
+                HStack{
+                    Button{
+                        selectTahun = true
+                        selectHari = false
+                        selectDefault = "tahun"
+                        //set data rek
+                        oneway = dataRekayasa["tahun"]["one_way"].stringValue
+                        contraflow = dataRekayasa["tahun"]["contra_flow"].stringValue
+                        pengalihan = dataRekayasa["tahun"]["pengalihan"].stringValue
+                        
+                    }label:{
+                        Image(systemName: selectTahun ? "checkmark.circle.fill" : "circle")
+                            .font(.system(size: 20))
+                            .foregroundColor(selectTahun ? Color.blue : Color.gray)
+                    }
+                    Text("Tahun")
+                        .font(.system(size: 13, weight: .bold))
+                    
+                    Button{
+                        selectTahun = false
+                        selectHari = true
+                        selectDefault = "hari"
+                        //set data rek
+                        oneway = dataRekayasa["hari"]["one_way_daily"].stringValue
+                        contraflow = dataRekayasa["hari"]["contra_flow_daily"].stringValue
+                        pengalihan = dataRekayasa["hari"]["pengalihan_daily"].stringValue
+                        
+                    }label:{
+                        Image(systemName: selectHari ? "checkmark.circle.fill" : "circle")
+                            .font(.system(size: 20))
+                            .foregroundColor(selectHari ? Color.blue : Color.gray)
+                    }
+                    Text("Hari")
+                        .font(.system(size: 13, weight: .bold))
+                }
+            }
+            Text("Dashboard Rekayasa Lalu Lintas")
+                .font(.system(size: 13, weight: .bold))
+                .padding(.top, 5)
+                .padding(.bottom, 2)
+            
+            LazyVGrid(columns: columns, spacing: 20) {
+                VStack(alignment: .leading){
+                    HStack{
+                        Image("oneway")
+                            .font(.system(size: 25))
+                        Text(oneway)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.white)
+                        Spacer()
+                    }
+                    Text("One Way")
+                        .font(.system(size: 13, weight: .bold))
+                        .padding(.top, 5)
+                        .foregroundColor(Color.white)
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(UIColor(hexString: "#008D39")))
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                
+                VStack(alignment: .leading){
+                    HStack{
+                        Image("contraflow")
+                            .font(.system(size: 25))
+                        Text(contraflow)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.white)
+                        Spacer()
+                    }
+                    Text("Contra-Flow")
+                        .font(.system(size: 13, weight: .bold))
+                        .padding(.top, 5)
+                        .foregroundColor(Color.white)
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(UIColor(hexString: "#6D51CC")))
+                .cornerRadius(10)
+                .shadow(radius: 5)
+               
+                VStack(alignment: .leading){
+                    HStack{
+                        Image("pengalihan")
+                            .font(.system(size: 25))
+                        Text(pengalihan)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.white)
+                        Spacer()
+                    }
+                    Text("Pengalihan")
+                        .font(.system(size: 13, weight: .bold))
+                        .padding(.top, 5)
+                        .foregroundColor(Color.white)
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(UIColor(hexString: "#FF5400")))
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                
+            }
+            
+            Text("")
+                .font(.system(size: 13, weight: .bold))
+                .padding(.top, 5)
+                .padding(.bottom, 2)
+            Spacer()
+        }
+        .padding()
+        .onAppear{
+            
+            DashboardLalinModel().getDataDashRekayasa{ resdata in
+                dataRekayasa = resdata
+                
+                oneway = dataRekayasa["hari"]["one_way_daily"].stringValue
+                contraflow = dataRekayasa["hari"]["contra_flow_daily"].stringValue
+                pengalihan = dataRekayasa["hari"]["pengalihan_daily"].stringValue
+            }
+        }
     }
 }
 
